320d0b0973b721bd048ff402942aec26f55a7414
MahmoudsNasr77 <67973033+MahmoudsNasr77@users.noreply.github.com>
2020-07-13 22:49:29 +0200

    Add files via upload

---
+++ z.js
@@ -0,0 +1,38 @@
+$(function () {
+    "use strict";
+    $("#img1").click(function () {
+    $("#img1").slideUp().slideDown(); 
+    });
+    $("#img2").click(function () {
+        $("#img2").slideUp().slideDown(); 
+    });
+    
+    $("#img3").click(function () {
+        $("#img3").slideUp().slideDown(); 
+    });
+    $("#k1").click(function () {
+        $("#k1").fadeOut().fadeIn(); 
+    });
+    $("#k2").click(function () {
+        $("#k2").fadeOut().fadeIn(); 
+    });
+    $("#k3").click(function () {
+        $("#k3").fadeOut().fadeIn(); 
+    });
+    $("#k4").click(function () {
+        $("#k4").fadeOut().fadeIn(); 
+    });
+    $("#k5").click(function () {
+        $("#k5").fadeOut().fadeIn(); 
+    });
+    $("#k6").click(function () {
+        $("#k6").fadeOut().fadeIn(); 
+    });
+    $(".div2").click(function () {
+        $(".div2").slideUp().slideDown(); 
+    });
+    
+
+    
+    
+});
